7ccfdf231ba99b69b1e25d8026ad50f62810e984
sproshchaev <sproshchaev@gmail.com>
2025-09-27 17:02:51 +0500

    init

---
+++ src/main/java/com/prosoft/spring_minikube_demo/SpringMinikubeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.prosoft.spring_minikube_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMinikubeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMinikubeDemoApplication.class, args);
+	}
+
+}
